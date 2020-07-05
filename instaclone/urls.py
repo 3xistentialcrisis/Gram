b5dfacb1f85b310d8fb92b5358883ef16d441e39
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 23:51:54 +0300

    Comment out static urlpatterns settings

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 app_name = 'instaclone'
 
@@ -13,5 +13,5 @@ urlpatterns = [
     url(r'profile/<username>/', views.profile, name='profile'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
