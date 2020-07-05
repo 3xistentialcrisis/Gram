c3c99571d86838512cc5362609767375e1182453
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 23:39:27 +0300

    Create Update User Form and Update User Profile Fprm Classes

--- instaclone/forms.py
+++ instaclone/forms.py
@@ -9,7 +9,20 @@ class SignUpForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ('username', 'email', 'password1', 'password2')
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Kindly provide a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class UpdateUserProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_picture', 'bio', 'name']
 
 class ProfileForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
