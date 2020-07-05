3b2db16fd6c8ad193dff3cdbb4066409faeaa282
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 22:47:27 +0300

    Add profile view to instaclone app url patterns

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^instaclone/', views.index, name='index'),
     url(r'signup/', views.signup, name='signup'),
     url(r'account/', include('django.contrib.auth.urls')),
-    url('', views.index, name='index')
+    url('', views.index, name='index'),
+    url(r'profile/', views.profile, name='profile'),
 ]
