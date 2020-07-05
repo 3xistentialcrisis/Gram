4ce2cc159039b448e93958dffcb5c6ed054d9912
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 19:39:39 +0300

    Add function to view index page

--- instaclone/views.py
+++ instaclone/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+#Index Page
+def index(request):
+    return render(request, 'instaclone/index.html')
 
+#Signup Page    
 def signup(request):
     return render(request, 'registration/signup.html')
