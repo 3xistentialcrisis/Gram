2015eb3e3190349b8d32eeb516bd9bd5a905217b
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 21:03:10 +0300

    Add Login_Redirect and Logout_Redirect to  project settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -150,3 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+LOGIN_REDIRECT_URL = 'index'
+
+LOGOUT_REDIRECT_URL = 'login'
