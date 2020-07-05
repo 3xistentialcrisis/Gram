a1431ff28cb3bc8c695ca1e8ed0886030891fa51
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-05 21:06:48 +0300

    Edit index url pattern and add account url pattern

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'instaclone'
 
 urlpatterns = [
-    url(r'^instaclone/', views.index, name='index'),
-    url(r'signup/', views.signup, name='signup')
+    # url(r'^instaclone/', views.index, name='index'),
+    url(r'signup/', views.signup, name='signup'),
+    url(r'account/', include('django.contrib.auth.urls')),
+    url('', views.index, name='index')
 ]
