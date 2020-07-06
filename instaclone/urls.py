be18cda8b0e7488dee0c14ca80f87f3f704fa1a2
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-06 11:37:24 +0300

    Remove static settings from  App urls

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -1,8 +1,7 @@
 from django.conf.urls import url, include
 from . import views
 from instaclone.views import PostLikeToggle, PostLikeAPIToggle
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 app_name = 'instaclone'
 
@@ -21,7 +20,3 @@ urlpatterns = [
     url(r'unfollow/<to_unfollow>', views.unfollow, name='unfollow'),
     url(r'follow/<to_follow>', views.follow, name='follow'
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
