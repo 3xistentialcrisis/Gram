c5f69c55fcd1bdb8ad02e86057b7beaf633822ea
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-08 12:00:49 +0300

    deploy to heroku

--- instaclone/views.py
+++ instaclone/views.py
@@ -33,7 +33,7 @@ def index(request):
         'users': users,
 
     }
-    return render(request, 'instaclone/index.html', params)
+    return render(request, 'index.html', params)
 
 #Signup Page    
 def signup(request):
