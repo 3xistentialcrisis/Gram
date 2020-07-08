49a3f6f28e517ddb84fa78fffe27eac0347150cf
Wanjiku Karanja <ciiku92@gmail.com>
2020-07-08 11:44:27 +0300

    Deploy to Heroku

--- instaclone/views.py
+++ instaclone/views.py
@@ -45,7 +45,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('index.html')
+            return redirect('instaclone/index.html')
            
     else:
         form = SignUpForm()
